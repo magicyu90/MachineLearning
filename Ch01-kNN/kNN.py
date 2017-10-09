76e43411634ab11bdada0eef98a6cc6da3133f52
magicyu90 <magicyu90@gmail.com>
2017-10-09 17:08:58 +0800

    add Ch01-file2matrix function

--- Ch01-kNN/kNN.py
+++ Ch01-kNN/kNN.py
@@ -12,32 +12,45 @@ def createDataSet():
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    print('diffMat:', diffMat)
     sqlDiffMat = diffMat ** 2
-    print('sqlDiffMat:', sqlDiffMat)
     sqlDistance = sqlDiffMat.sum(axis=1)
-    print('sqlDistances:', sqlDistance)
     distances = sqlDistance**0.5
     sortedDistIndices = distances.argsort()
-
-    print('sortedDistIndices:', sortedDistIndices)
-
     classCount = {}
 
     for i in range(k):
         label = labels[sortedDistIndices[i]]
         classCount[label] = classCount.get(label, 0) + 1
-
-    print('classCount:', classCount)
     sortedClassCount = sorted(classCount.items(),
                               key=operator.itemgetter(1), reverse=True)
-
-    print('sortedClassCount:', sortedClassCount)
 
     res = sortedClassCount[0, 0]
     print('result:', res)
 
 
-group, labels = createDataSet()
+# group, labels = createDataSet()
 
-classify0([0.0, 0cxvcxxxxxxxxxxxxxxxxxxxx.0], group, labels, 3)
+# classify0([0, 0], group, labels, 3)
+
+
+def file2matrix(fileName):
+    fr = open(fileName)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMatrix = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMatrix[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+
+
+    print('returnMatrix:', returnMatrix, 'classLabelVector:', classLabelVector)
+    return returnMatrix, classLabelVector
+
+
+file2matrix('datingTestSet2.txt')
