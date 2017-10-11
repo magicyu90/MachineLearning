91ade8c6451ff3904440a5c62a7dd86a47bc9000
magicyu90 <magicyu90@gmail.com>
2017-10-11 21:24:23 +0800

    choose Best Feature To Split

--- Ch02-dcTree/trees.py
+++ Ch02-dcTree/trees.py
@@ -8,23 +8,24 @@ def createDataSet():
     return dataSet, labels
 
 
-def calcShanoonEnt(dataSet):
+def calcShannonEnt(dataSet):
     '''计算数据集的香农火商(entropy)'''
     numEntries = len(dataSet)
     labelCounts = {}
     for item in dataSet:
-        currentLabel = item[-1]
+        currentLabel = item[-1]  # 最后一列
         print('currentLabel:', currentLabel)
         if currentLabel not in labelCounts.keys():
             labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     print('labelCounts:', labelCounts)
 
-    shanoonEnt = 0.0
+    shannonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key]) / numEntries  # 出现概率
-        shanoonEnt -= prob * log(prob, 2)  # 计算火商(entropy)
-    print('shanoonEnt:', shanoonEnt)
+        shannonEnt -= prob * log(prob, 2)  # 计算火商(entropy)
+    print('shannonEnt:', shannonEnt)
+    return shannonEnt
 
 
 def splitDataSet(dataSet, axis, value):
@@ -35,5 +36,27 @@ def splitDataSet(dataSet, axis, value):
             reducedFeatVec = item[:axis]  # 得到空的[]
             reducedFeatVec.extend(item[axis + 1:])
             retDataSet.append(reducedFeatVec)
-
     print('retDataSet:', retDataSet)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    '''选择最好的数据集划分方式'''
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+
+    for i in range(numFeatures):  # i遍历每一个特征
+        featList = [example[i] for example in dataSet]
+        print('featList:', featList)
+        uniqueVals = set(featList)  # get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
--- Ch02-dcTree/treesMain.py
+++ Ch02-dcTree/treesMain.py
@@ -2,5 +2,6 @@ import trees as treesClass
 
 dataSet, labels = treesClass.createDataSet()
 
-# treesClass.calcShanoonEnt(dataSet)
-treesClass.splitDataSet(dataSet, 0, 1)
+# treesClass.calcShannonEnt(dataSet)
+# treesClass.splitDataSet(dataSet, 0, 1)
+treesClass.chooseBestFeatureToSplit(dataSet)
