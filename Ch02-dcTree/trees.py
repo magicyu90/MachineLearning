7e3cc8afd6a91066e753b9f7a473d3639af908ea
magicyu90 <magicyu90@gmail.com>
2017-10-11 20:20:55 +0800

    add split data set

--- Ch02-dcTree/trees.py
+++ Ch02-dcTree/trees.py
@@ -1,7 +1,15 @@
 from math import log
 
 
+def createDataSet():
+    '''创建测试数据集'''
+    dataSet = [[1, 1, 'y'], [1, 1, 'y'], [1, 0, 'n'], [0, 1, 'n'], [0, 1, 'n']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
 def calcShanoonEnt(dataSet):
+    '''计算数据集的香农火商(entropy)'''
     numEntries = len(dataSet)
     labelCounts = {}
     for item in dataSet:
@@ -15,11 +23,17 @@ def calcShanoonEnt(dataSet):
     shanoonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key]) / numEntries  # 出现概率
-        shanoonEnt -= prob * log(prob, 2)  # 计算火商
+        shanoonEnt -= prob * log(prob, 2)  # 计算火商(entropy)
     print('shanoonEnt:', shanoonEnt)
 
 
-def createDataSet():
-    dataSet = [[1, 1, 'y'], [1, 0, 'n'], [0, 1, 'n'], [0, 1, 'n']]
-    labels = ['no surfacing', 'flippers']
-    return dataSet, labels
+def splitDataSet(dataSet, axis, value):
+    '''按照给定特征划分数据集'''
+    retDataSet = []
+    for item in dataSet:
+        if item[axis] == value:
+            reducedFeatVec = item[:axis]  # 得到空的[]
+            reducedFeatVec.extend(item[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+
+    print('retDataSet:', retDataSet)
--- Ch02-dcTree/treesMain.py
+++ Ch02-dcTree/treesMain.py
@@ -2,4 +2,5 @@ import trees as treesClass
 
 dataSet, labels = treesClass.createDataSet()
 
-treesClass.calcShanoonEnt(dataSet)
+# treesClass.calcShanoonEnt(dataSet)
+treesClass.splitDataSet(dataSet, 0, 1)
