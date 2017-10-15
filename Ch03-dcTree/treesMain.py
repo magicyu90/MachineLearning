7c29fd2295e948d11131d1da37124aa509d87ae6
magicyu90 <magicyu90@gmail.com>
2017-10-15 10:50:41 +0800

    change folder name

--- Ch03-dcTree/treesMain.py
+++ Ch03-dcTree/treesMain.py
@@ -1,8 +1,12 @@
 import trees as treesClass
+import treePlotter
+
 
 dataSet, labels = treesClass.createDataSet()
 
 # treesClass.calcShannonEnt(dataSet)
 # treesClass.splitDataSet(dataSet, 0, 1)
 # treesClass.chooseBestFeatureToSplit(dataSet)
-treesClass.createTree(dataSet, labels)
+# treesClass.createTree(dataSet, labels)
+
+treePlotter.createPlot()
