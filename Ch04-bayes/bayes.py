0dc4a2b48258b47e615b11f702b5d91554e8044b
magicyu90 <magicyu90@gmail.com>
2017-10-24 19:49:26 +0800

    change trainNB0 with log

--- Ch04-bayes/bayes.py
+++ Ch04-bayes/bayes.py
@@ -17,6 +17,7 @@ def createVocabList(dataSet):
     vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
+
     vocabList = list(vocabSet)
     sortedList = sorted(vocabList, key=lambda x: x.lower())
     return sortedList
@@ -27,6 +28,7 @@ def setOfWords2Vec(vocabList, inputSet):
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
+
     print('returnVec:', returnVec)
     return returnVec
 
@@ -43,10 +45,10 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num = zeros(numWords)
-    p1Num = zeros(numWords)
-    p0Denom = 0.0
-    p1Denom = 0.0
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -54,8 +56,8 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p1Denom
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p1Denom)
 
     print('p1Num:', p1Num, 'p1Denom:', p1Denom)
     print('p0Num:', p0Num, 'p0Denom:', p0Denom)
@@ -63,32 +65,6 @@ def trainNB0(trainMatrix, trainCategory):
     print('p0Vect:', p0Vect)
     print('p1Vect:', p1Vect)
     return p0Vect, p1Vect, pAbusive
-
-
-def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # element-wise mult
-    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
-    if p1 > p0:
-        return 1
-    else:
-        return 0
-
-
-def testingNB():
-    listOfPosts, listClass = loadDataSet()
-    myVocablist = createVocabList(listOfPosts)
-    trainMat = []
-    for postinDoc in listOfPosts:
-        trainMat.append(setOfWords2Vec(myVocablist, postinDoc))
-    p0v, p1v, pAb = trainNB0(array(trainMat), array(listClass))
-    testEntry = ['love', 'my', 'dalmation']
-    thisDoc = array(setOfWords2Vec(myVocablist, testEntry))
-    print('thisDoc:', thisDoc)
-    print(testEntry, 'classfied as:', classifyNB(thisDoc, p0v, p1v, pAb))
-    testEntry = ['stupid', 'garbage']
-    thisDoc = array(setOfWords2Vec(myVocablist, testEntry))
-    print('thisDoc:', thisDoc)
-    print(testEntry, 'classfied as:', classifyNB(thisDoc, p0v, p1v, pAb))
 
 
 def textParse(bigStr):
@@ -104,14 +80,11 @@ def spamTest():
     classList = []
     fullText = []
     for i in range(1, 26):
-        print('i:', i)
-        wordList = textParse(open('email/spam/%d.txt' %
-                                  i, "r", encoding='utf-8', errors='ignore').read())
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(1)
-        wordList = textParse(open('email/ham/%d.txt' %
-                                  i, "r", encoding='utf-8', errors='ignore').read())
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
@@ -135,12 +108,3 @@ def spamTest():
         item = bagOfWords2VecMN(vocabList, docList[docIndex])
         trainMat.append(item)
         trainClasses.append(classList[docIndex])
-    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
-    errorCount = 0
-
-    for docIndex in testSet:
-        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
-        if(classifyNB(array(wordVector), p0V, p1V, pSpam)) != classList[docIndex]:
-            errorCount += 1
-
-    print('the error rate is:', float(errorCount / len(testSet)))
